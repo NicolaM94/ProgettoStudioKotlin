2594fe8e979883a9f0e4742672f149f670ee1420
NicolaM94 <nicola.moro2312@gmail.com>
2020-09-26 19:25:12 +0200

    Adding more functions

--- src/main/kotlin/Classes/BalanceCollector.kt
+++ src/main/kotlin/Classes/BalanceCollector.kt
@@ -3,6 +3,7 @@ package Classes
 import kotlin.math.floor
 import java.io.File
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import kotlin.math.cos
 
 
 class BalanceCollector (pathToBalance:String) {
@@ -10,10 +11,10 @@ class BalanceCollector (pathToBalance:String) {
     private val path = File(pathToBalance)
     private val reader = XSSFWorkbook(path)
 
-    val activeMap: MutableMap<Double, Active> = mutableMapOf()
-    val passiveMap: MutableMap<Double,Passive> = mutableMapOf()
-    val costsMap: MutableMap<Double,Cost> = mutableMapOf()
-    val revenuesMap: MutableMap<Double,Revenue> = mutableMapOf()
+    private val activeMap: MutableMap<Double, Active> = mutableMapOf()
+    private val passiveMap: MutableMap<Double,Passive> = mutableMapOf()
+    private val costsMap: MutableMap<Double,Cost> = mutableMapOf()
+    private val revenuesMap: MutableMap<Double,Revenue> = mutableMapOf()
 
     /**Collecting values from the balance*/
     init {
@@ -329,7 +330,7 @@ class BalanceCollector (pathToBalance:String) {
         }
         return result
     }
-    fun accrualsAndDeferrals () :Double {
+    fun activeAccrualsAndDeferrals () :Double {
         var result :Double = 0.00
         activeMap.forEach { el ->
             if (floor(el.key).toInt() in 19110..192220) result += el.value.value
@@ -338,7 +339,6 @@ class BalanceCollector (pathToBalance:String) {
     }
 
     /**Passive MicroAreas*/
-
     fun netWealth () :Double {
         var result :Double = 0.00
         passiveMap.forEach { el ->
@@ -351,7 +351,235 @@ class BalanceCollector (pathToBalance:String) {
         }
         return result
     }
+    fun faPlantCosts () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 21010..21040) result += el.value.value
+        }
+        return result
+    }
+    fun faReDCosts () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 21110..21130) result += el.value.value
+        }
+        return result
+    }
+    fun faPatents () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 21210..21242) result+=el.value.value
+        }
+        return result
+    }
+    fun faLicenes () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 21310..21350) result += el.value.value
+        }
+        return result
+    }
+    fun faGoodWill () :Double {
+        var result = passiveMap[21410.1]?.value ?: 0.00
+        return result * -1
+    }
+    fun faOtherFixedAssets () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach{el ->
+            if (floor(el.key).toInt() in 21610..21670) result += el.value.value
+        }
+        return result
+    }
+    fun faFieldsAndEstate () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 22010..22030) result += el.value.value
+        }
+        return result
+    }
+    fun faMachineriesAndImplants () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach{el ->
+            if (floor(el.key).toInt() in 22111..22122) result+=el.value.value
+        }
+        return result
+    }
+    fun faEquipments () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 22210..22230) result += el.value.value
+        }
+        return result
+    }
+    fun faOtherGoods () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 22310..22360) result+=el.value.value
+        }
+        return result
+    }
+    fun riskFunds () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 22510..24350 || floor(el.key).toInt() in 24510..24799) result+=el.value.value
+        }
+        return result
+    }
+    fun severanceIndemnities () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 24410..24499 || floor(el.key).toInt() == 24810) result += el.value.value
+        }
+        return result
+    }
+    fun longDebts () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach{el->
+            val test :Int = floor(el.key).toInt()
+            if (test in 24910..25120 || test == 25260 || test in 25310..25399) result += el.value.value
+        }
+        return result
+    }
+    fun shortDebts () :Double {
+        var result :Double = 0.00
+        passiveMap.forEach { el ->
+            val test :Int = floor(el.key).toInt()
+            if (test in 25210..25299 && test != 25260 || test in 25411..26599) result+=el.value.value
+        }
+        return result
+    }
+    fun taxDebts () :Double {
+        var result:Double = 0.00
+        passiveMap.forEach { el ->
+            if (floor(el.key).toInt() in 27010..28050) result+=el.value.value
+        }
+        return result
+    }
+    fun employeesAndAdministrators () :Double {
+        var result: Double = 0.00
+        passiveMap.forEach { el->
+            if (floor(el.key).toInt() in 28110..28399) result+=el.value.value
+        }
+        return result
+    }
+    fun clientsDebts () :Double {
+        var result: Double = 0.00
+        passiveMap.forEach{el ->
+            if (floor(el.key).toInt() in 28410..28499) result += el.value.value
+        }
+        return result
+    }
+    fun associatesDebts () :Double {
+        var result = 0.00
+        passiveMap.forEach{el ->
+            if (floor(el.key).toInt() in 28511..28599) result+= el.value.value
+        }
+        return result
+    }
+    fun otherDebts () :Double {
+        var result:Double = 0.00
+        passiveMap.forEach{el->
+            if (floor(el.key).toInt() in 28610..29010) result+=el.value.value
+        }
+        return result
+    }
+    fun passiveAccrualsAndDeferrals () :Double {
+        var result = 0.00
+        passiveMap.forEach{el ->
+            if (floor(el.key).toInt() in 29110..29230) result+=el.value.value
+        }
+        return result
+    }
 
+    /**Costs MicroAreas*/
+    fun suppliesAndBurdens () :Double {
+        var result:Double = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 30010..30199) result += el.value.value
+            else if (floor(el.key).toInt() in 30210..30235) result += el.value.value
+        }
+        return result
+    }
+    fun industrialServices () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 31011..31099) result += el.value.value
+        }
+        return result
+    }
+    fun commercialServices () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 31110..31199) result += el.value.value
+        }
+        return result
+    }
+    fun utilities () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 31211..31299) result+=el.value.value
+        }
+        return result
+    }
+    fun adminAndCollaboratos () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 31310..31799) result+=el.value.value
+        }
+        return result
+    }
+    fun otherServices () :Double {
+        var result :Double = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 31810..31899) result += el.value.value
+            else if (floor(el.key).toInt() in 31910..31989) result -= el.value.value
+        }
+        return result
+    }
+    fun mortgageAndLeasings () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 32010..32399) result += el.value.value
+            else if (floor(el.key).toInt() == 32410) result -= el.value.value
+        }
+        return result
+    }
+    fun employeesAndSocialCost () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 33010..33499) result += el.value.value
+        }
+        return result
+    }
+    fun amortizations () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 34001..34136) result+=el.value.value
+        }
+        return result
+    }
+    fun depreciations () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 34201..34799) result+=el.value.value
+        }
+        return result
+    }
+    fun taxesAndRights () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 35001..35199) result+=el.value.value
+        }
+        return result
+    }
+    fun otherCosts () :Double {
+        var result = 0.00
+        costsMap.forEach { el ->
+            if (floor(el.key).toInt() in 35201..35299) result+=el.value.value
+        }
+        result -= costsMap[35310.1]?.value ?:0.00
+        return result
+    }
 
 
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,6 +11,11 @@ fun main() {
         |Crediti v. soci: ${b.creditsVsAssociates()}
         |Costi di impianto: ${b.plantCosts()} 
         |Patrimonio netto: ${b.netWealth()}
+        |Debiti a lungo termine: ${b.longDebts()}
+        |DEbiti a corto termine: ${b.shortDebts()}
+        |Risconti e ratei attivi: ${b.activeAccrualsAndDeferrals()}
+        |Risconti e ratei passivi: ${b.passiveAccrualsAndDeferrals()}
+        |aaaa: ${b.associatesDebts()}
         """.trimMargin())
 
 
